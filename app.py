cee2059c9c9e618971c843e94b5a28fce08d7050
Michal <michal.mencfel@gmail.com>
2024-03-20 19:03:27 +0100

    create logger

--- app.py
+++ app.py
@@ -1,1 +1,18 @@
-print('HI world')
+import logging
+from common import start_logger
+
+start_logger()
+logger = logging.getLogger()
+
+
+
+
+
+def set_up():
+    logging.info('START SYSTEM')
+
+
+
+
+if __name__ == "__main__":
+    set_up()
