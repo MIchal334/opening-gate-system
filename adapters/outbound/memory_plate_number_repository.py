59c8532ed230d42852248752ec2b21971f124218
Michal <michal.mencfel@gmail.com>
2024-03-20 19:44:56 +0100

    add test for memory plate repo

--- adapters/outbound/memory_plate_number_repository.py
+++ adapters/outbound/memory_plate_number_repository.py
@@ -1,13 +1,17 @@
-from application.ports.outbound import PlateNumberRepository
+from application.ports.outbound.plate_data_repo import PlateNumberRepository
+from typing import List
 
 class MemoryPlateNumberRepository(PlateNumberRepository):
     
     def __init__(self):
         self.plate_list = []
     
-    def get_all_plate_number(self) -> list[str]:
+    def get_all_plate_number(self) -> List[str]:
         return self.plate_list
 
-    def update_plate_number(self,plate_number_list: list[str]) -> None:
-        self.plate_list.clear()
+    def update_plate_number(self,plate_number_list: List[str]) -> None:
+        self.clear_all()
         self.plate_list.extend(plate_number_list)
+
+    def clear_all(self) -> None:
+        self.plate_list.clear()
--- application/ports/outbound/plate_data_repo.py
+++ application/ports/outbound/plate_data_repo.py
@@ -1,12 +1,14 @@
 from abc import ABC, abstractmethod
+from typing import List
+
 
 class PlateNumberRepository(ABC):
     
     @abstractmethod
-    def get_all_plate_number(self) -> list[str]:
+    def get_all_plate_number(self) -> List[str]:
         pass
 
     @abstractmethod
-    def update_plate_number(self,plate_number_list: list[str]) -> None:
+    def update_plate_number(self,plate_number_list: List[str]) -> None:
         pass    
     
