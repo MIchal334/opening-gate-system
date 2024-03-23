88467987f5b29df1d264b47d247d062f25be6d83
Michał Mencfel <mihal30060@gmail.com>
2024-03-23 10:28:21 +0000

    Add flask server and plate repository

---
+++ application/ports/outbound/plate_data_repo.py
@@ -0,0 +1,17 @@
+from abc import ABC, abstractmethod
+from typing import List
+
+
+class PlateNumberRepository(ABC):
+    
+    @abstractmethod
+    def get_all_plate_number(self) -> List[str]:
+        pass
+
+    @abstractmethod
+    def update_plate_number(self,plate_number_list: List[str]) -> None:
+        pass    
+    
+    @abstractmethod
+    def clear_all(self) -> None:
+        pass
---
+++ common/__init__.py
@@ -0,0 +1,15 @@
+import logging
+from logging.handlers import RotatingFileHandler
+
+
+
+def start_logger():
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(formatter)
+    logger.addHandler(console_handler)
+    file_handler = RotatingFileHandler('app.log', maxBytes=10*1024*1024, backupCount=3)
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
