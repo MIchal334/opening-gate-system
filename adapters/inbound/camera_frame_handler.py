3b76f7220a639194a92901d47be3ffdbab55ef76
Michal Mencfel <269996@student.pwr.edu.pl>
2024-04-11 15:17:04 +0200

    adjustemnt after tests

--- adapters/inbound/camera_frame_handler.py
+++ adapters/inbound/camera_frame_handler.py
@@ -10,25 +10,18 @@ logger = logging.getLogger()
 class CameraFrameHandler(FrameHandler):
     def __init__(self, p_camera_link) -> None:
         self.camera_link = p_camera_link
-        self.capturer = cv2.VideoCapture(p_camera_link)
-        self.frames = []
-        self.DELAY_SECONDS = 250
-        self.last_time = int(1000*time.time())
+        self.capturer = cv2.VideoCapture(self.camera_link)
 
     def get_frame(self) -> 'numpy.ndarray'|None:
         ret, frame = self.capturer.read()
+        
+        if not ret:
+            logger.info(f"End of stream camera {self.camera_link}")
+            self.capturer = cv2.VideoCapture(self.camera_link)
 
         if frame is not None:
             logger.info(f'Frame is not None from: {self.camera_link}')
-            self.frames.append(frame)
-            # return self.frames[-1]
-            print(f'DIFFEENCE {int(1000*time.time()) - self.last_time }')
-            if int(1000*time.time()) - self.last_time > self.DELAY_SECONDS :
-                logger.info('GET FRAME FORM INDOOR FAKE')
-                self.last_time = int(1000*time.time())
-                return self.frames[-1]
+            return frame
         
         logger.debug(f'Frame is None from: {self.camera_link}')
         return None
-        
-            
